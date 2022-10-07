9dade885901df56b2cbdc5280be3be88ae24d4b2
AlexBotaefja <112579750+AlexBotaefja@users.noreply.github.com>
2022-10-07 02:25:19 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.Prefix = `>>`;
-exports.Token = `MTAxNDIxNTM0MTIwOTAzODg5OQ.GVaDqy.b3dkIIn5h1DKe3Jir0-MhuRxBnzwPlowdvm_LE`;
+exports.Token = `MTAxNDIxNTM0MTIwOTAzODg5OQ.GVaDqy.b3dkIIn5h1DKe3Jir0MhuRxBnzwPlowdvm_LE`;
 exports.Color = `#00ffe3`;
 exports.Port = `8080`;
 exports.BotName= `GracieBot`;
